6cc6a6c761c92e53e89767c598b5214f0a69014b
Awais <m.awaisnaxeer@gmail.com>
2021-04-03 12:10:16 +0500

    Deploying and deleting functions

--- functions/index.js
+++ functions/index.js
@@ -13,58 +13,67 @@ exports.helloworld = functions.https.onRequest((req,res)=>{
     res.send("Hello from firebase functions...");
 })
 
-exports.api = functions.https.onRequest(async (req,res)=>{
-    switch(req.method){
-        case 'GET':
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
-            res.send(response.data);
-            break;
+// exports.api = functions.https.onRequest(async (req,res)=>{
+//     switch(req.method){
+//         case 'GET':
+//             const response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+//             res.send(response.data);
+//             break;
 
-        case 'POST':
-            const body = req.body
-            res.send(body);
-            break;
+//         case 'POST':
+//             const body = req.body
+//             res.send(body);
+//             break;
 
-        case 'DELETE':
-            res.send('it was DELETE request');
-            break;
+//         case 'DELETE':
+//             res.send('it was DELETE request');
+//             break;
 
-        default:
-            res.send('it was default request');
-            break;
-    }
-})
-
-
-exports.userAdded = functions.auth.user().onCreate(user =>{
-    console.log(`${user.email} is created`)
-    return Promise.resolve()
-})
+//         default:
+//             res.send('it was default request');
+//             break;
+//     }
+// })
 
 
-exports.userDeleted = functions.auth.user().onDelete(user =>{
-    console.log(`${user.email} is deleted`)
-    return Promise.resolve()
-})
-
-exports.fruitAdded = functions.firestore.document('/fruits/{documentId}').onCreate((snapshot,context)=>{
-    console.log(snapshot.data(), 'created');
-    return Promise.resolve();
-})
-
-exports.fruitDeleted = functions.firestore.document('/fruits/{documentId}').onDelete((snapshot,context)=>{
-    console.log(snapshot.data(), 'deleted');
-    return Promise.resolve();
-})
-
-exports.fruitUpdated = functions.firestore.document('/fruits/{documentId}').onUpdate((snapshot,context)=>{
-    console.log(snapshot.before.data(), 'Before');
-    console.log(snapshot.after.data(), 'updated');
-    return Promise.resolve();
-})
+// exports.userAdded = functions.auth.user().onCreate(user =>{
+//     console.log(`${user.email} is created`)
+//     return Promise.resolve()
+// })
 
 
-exports.scheduleFunction = functions.pubsub.schedule('* * * * *').onRun(context =>{
-    console.log("I'm running/executing every minute...")
-    return null;
-})
+// exports.userDeleted = functions.auth.user().onDelete(user =>{
+//     console.log(`${user.email} is deleted`)
+//     return Promise.resolve()
+// })
+
+// exports.fruitAdded = functions.firestore.document('/fruits/{documentId}').onCreate((snapshot,context)=>{
+//     console.log(snapshot.data(), 'created');
+//     return Promise.resolve();
+// })
+
+// exports.fruitDeleted = functions.firestore.document('/fruits/{documentId}').onDelete((snapshot,context)=>{
+//     console.log(snapshot.data(), 'deleted');
+//     return Promise.resolve();
+// })
+
+// exports.fruitUpdated = functions.firestore.document('/fruits/{documentId}').onUpdate((snapshot,context)=>{
+//     console.log(snapshot.before.data(), 'Before');
+//     console.log(snapshot.after.data(), 'updated');
+//     return Promise.resolve();
+// })
+
+
+// exports.scheduleFunction = functions.pubsub.schedule('* * * * *').onRun(context =>{
+//     console.log("I'm running/executing every minute...");
+//     return null;
+// })
+
+
+/*
+Deploying Functions....
+ firebase deploy --only functions
+  firebase deploy --only functions:helloworld // to deploy only one specific function
+  firebase deploy --only "functions:scheduleFunction"
+
+*/
